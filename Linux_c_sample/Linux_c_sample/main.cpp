8288c8cb7ff19cbc9a1adb502fbde67ab4cc1fd0
LIiliILf <3162441851@qq.com>
2023-09-22 20:21:43 +0800

    0922完成第三次课0916的完善功能1

--- Linux_c_sample/Linux_c_sample/main.cpp
+++ Linux_c_sample/Linux_c_sample/main.cpp
@@ -7,10 +7,14 @@
 #include <pthread.h>
 #include <dirent.h> //opendir
 
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
 #define MSG_TYPE_LOGIN 0
 #define MSG_TYPE_FILENAME 1
+#define MSG_TYPE_DOWNLOAD 2
+
 //通讯协议的定制，两边要保持一致，拷贝过去
 typedef struct msg
 {
@@ -18,6 +22,7 @@ typedef struct msg
     int flag; //标志位
     char buffer[128]; //存放除文件名之外的内容
     char fname[50]; //如果type是1，文件名传输包，那么fname里面就存放着文件名
+    //int bytes;  //这个字段用来记录传输文件时每个数据包实际的文件字节数
 }MSG; //这个结构体会根据业务需求的不断变化，添加新的字段
 
 //根据网盘客户端的业务需求，客户端想要查看服务器这边目录下的文件名信息。
@@ -28,11 +33,9 @@ void search_server_dir(int accept_socket)//把套接字作为参数传进来
 {
     //opendir是打开linux目录的api函数
     struct dirent* dir = NULL; //定义一个存放目录信息的结构体
-
     MSG info_msg = {0};
     int res =0;
     DIR* dp = opendir("/home/linux");
-    
     info_msg.type = MSG_TYPE_FILENAME;
     if (NULL == dp)
     {
@@ -60,7 +63,6 @@ void search_server_dir(int accept_socket)//把套接字作为参数传进来
                 return;
             }
         }
-
     }
 }
 
@@ -71,41 +73,47 @@ void* thread_fun(void* arg)
     int acpt_socket = *((int*)arg); //强制转换成指针
     int res;
     char buffer[50] = { 0 };
-
-    search_server_dir(acpt_socket);
+    MSG recv_msg = { 0 };   //从buffer缓冲区转换成结构体大小
+    // 0916_0848 等到按查询文件的时候才 search 注释掉,如果在这里调用那么不输入1就直接打印出目录了
+    //search_server_dir(acpt_socket);
     printf("目录信息发送给客户端完成\n");
-
-
     while (1)
     {
         //read函数就是接收客户端发来的数据，返回值表示实际从客户端那边收到的字节数。
         //buffer就是收到客户端数据后把数据存放的地址，sizeof(buffer)就是希望读取的字节数。
-        res = read(acpt_socket, buffer, sizeof(buffer));
-        printf("client read %s\n", buffer);
-        write(acpt_socket, buffer, res);
-        memset(buffer, 0, sizeof(buffer));
+        res = read(acpt_socket, &recv_msg, sizeof(MSG));
+        //0916_08:44服务器read问题  说明客户端已经断开服务器的连接了，read默认情况下他是阻塞模式
+        if (res == 0) {
+            printf("客户端已经断开\n");
+            break;
+        }
+        if (recv_msg.type == MSG_TYPE_FILENAME)//说明客户端发过来的信息要的是目录
+        {
+            search_server_dir(acpt_socket);
+            memset(&recv_msg, 0, sizeof(MSG));
+        }
+        //printf("client read %s\n", buffer); //0916 0903
+        //write(acpt_socket, buffer, res);
+        //memset(buffer, 0, sizeof(buffer));
+        memset(&recv_msg, 0, sizeof(MSG));
+
+        
         //服务器收到客户端数据后，原封不动把数据再回发给客户端。
     }
 }
-
-
 
 int main()
 {
     char buffer[50] = { 0 };
     int res = 0;
     //1.买手机 2买电话卡 3 电话卡插入手机 4 带上手机接收
-
     printf("开始创建tcp服务器\n");
     int server_socket; //socket网络描述符，也叫做套接字描述符，用于向网络发送数据。
     int accept_socket;
-
     pthread_t thread_id; //线程编号
-
     //1.创建套接字描述符
     server_socket = socket(AF_INET, SOCK_STREAM, 0);//以后要想向互联网发送数据都使用server socket这个套接字描述符
     //只要是流式都一样的步骤
-
     if (server_socket < 0) 
     {
         perror("socket create failed:");
@@ -115,14 +123,12 @@ int main()
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY; //INADDR_ANY宏定义，告诉系统自动绑定网卡ip
-    server_addr.sin_port = htons(7777);//通过网络地址转换协议htons,把主机字节顺序转换成网络字节顺序
+    server_addr.sin_port = htons(6666);//通过网络地址转换协议htons,把主机字节顺序转换成网络字节顺序
 
     //如果我们服务器程序退出后，又立刻打开服务器程序，系统会提示 Address already in used.
     //因为ip地址和端口号是资源资源，必须设置为端口号可以重复使用
     int optvalue = 1;
     setsockopt(server_socket,SOL_SOCKET,SO_REUSEADDR,&optvalue,sizeof(optvalue));
-
-
 
     //3.把我们设定好的ip地址和端口号绑定到我们的server_socket描述符上。 查 bind
     if (bind(server_socket, (struct sockaddr*) & server_addr, sizeof(server_addr)) < 0)
@@ -149,7 +155,6 @@ int main()
         //把accept放到while里面，然后创建线程
         //创建成功之后，系统就会执行thread_fun里面的代码。而这里面的代码是多线程代码（会和主线程代码一起执行）
         pthread_create(&thread_id, NULL, thread_fun, &accept_socket);   //thread_fun函数写在上面
-        
     }
     printf("%s 向你问好!\n", "Linux_c_sample");
     return 0;
--- Linux_disk_net_client/Linux_disk_net_client/main.cpp
+++ Linux_disk_net_client/Linux_disk_net_client/main.cpp
@@ -3,9 +3,11 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #define MSG_TYPE_LOGIN 0
 #define MSG_TYPE_FILENAME 1
+#define MSG_TYPE_DOWNLOAD 2
 //通讯协议的定制，两边要保持一致，拷贝
 typedef struct msg
 {
@@ -15,11 +17,8 @@ typedef struct msg
     char fname[50]; //如果type是1，文件名传输包，那么fname里面就存放着文件名
 }MSG; //这个结构体会根据业务需求的不断变化，添加新的字段
 
-//网盘
 void net_disk_ui()
 {
-    //在服务器连接成功之后调用一下
-    system("clear");
     printf("==========TCP网盘客户端================\n");
     printf("============功能菜单===================\n");
     printf("\t\t\t1.查询文件\n");
@@ -27,20 +26,38 @@ void net_disk_ui()
     printf("\t\t\t3.上传文件\n");
     printf("\t\t\t0.退出系统\n");
     printf("-----------------------------------------\n");
-    printf("请选择对应的操作");
-    
+    printf("请选择对应的操作:");
 }
 
+//client也要有个创建线程。接收服务器发送过来的数据 0916_0853
+void* thread_func(void* arg)
+{
+    int clinet_socket = *((int*)arg);
+    MSG recv_msg = { 0 };
+    int res;
+    while (1)
+    {
+        //用来接收服务器端发过来的数据
+        res = read(clinet_socket, &recv_msg, sizeof(MSG));
 
+        if (recv_msg.type == MSG_TYPE_FILENAME)
+        {
+            printf("server path filename%s \n", recv_msg.fname);
+            memset(&recv_msg, 0, sizeof(MSG));
+        }
+    }
+}
 int main()
 {
     int client_socket;
+    pthread_t thread_id;
     struct sockaddr_in server_addr; //用来填写要连接的服务器的ip地址和端口号
     char buffer[50] = {0};
     int res;
-
-    MSG recv_msg = { 0 };
-
+    char c; //用来保存用户在键盘上输入的字符
+    //0916_0859把recv改成send，为什么不是单独创建一个send
+    //MSG recv_msg = { 0 };
+    MSG send_msg = { 0 };
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket < 0)
     {
@@ -50,7 +67,7 @@ int main()
     
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("192.168.226.16"); //一种是直接指定，另一种如果s和c在同一台电脑中可以用127.0.0.1
-    server_addr.sin_port = htons(7777);
+    server_addr.sin_port = htons(6666);
 
     //创建好套接字之后，客户端要连接到服务器，这时候使用connect连接服务器 man connect
     if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)    //& server_addr 取地址，然后强制转换
@@ -58,12 +75,38 @@ int main()
         perror("connect error");
         return 0;
     }
-
-
     printf("client连接server成功\n");
-    net_disk_ui();
+    //连接成功之后创建线程，对应创建线程id
+    pthread_create(&thread_id, NULL, thread_func, &client_socket);
 
-
+    //0916_9点02
+    while (1)
+    {
+        net_disk_ui();
+        c = getchar(); //获取键盘输入
+        switch (c)
+        {
+        case '1':
+            //要让服务器给我们发送目录信息
+            //这个while循环本身也是死循环，所以客户端也要创建线程，让接收服务器数据的代码放进线程里。
+            send_msg.type = MSG_TYPE_FILENAME;
+            res = write(client_socket, &send_msg, sizeof(MSG));
+            if (res < 0)
+            {
+                perror("send msg error:");
+            }
+            memset(&send_msg, 0, sizeof(MSG));
+            break;
+        case '2':
+            /*send_msg.type = MSG_TYPE_DOWNLOAD;
+            res = write(client_socket, &send_msg, sizeof(MSG));*/
+            break;
+        case '3':
+            break;
+        case '0':
+            return 0;
+        }
+    }
     //客户端往服务器发送数据，用户再客户端终端连续输入字符串，回车表示发送数据。用while循环
     //while (fgets(buffer, sizeof(buffer), stdin ) != NULL )
     //{
@@ -75,8 +118,8 @@ int main()
     //    memset(buffer, 0, sizeof(buffer));
     //}
     
-    //改造
-   
+    //改造 0916为什么又没有这一段了。上课没有说，但是翻到这里看到没有
+   /*
     while (1)
     {
         res = read(client_socket, &recv_msg, sizeof(MSG));
@@ -88,5 +131,6 @@ int main()
     }
     
     printf("%s 向你问好!\n", "Linux_disk_net_client");
+    */
     return 0;
 }
