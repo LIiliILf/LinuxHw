2df24d05264661d882d74fe92877e84bdadf8c51
LIiliILf <3162441851@qq.com>
2023-10-12 22:45:21 +0800

    filename

--- Linux_netdisk_client/Linux_netdisk_client/main.cpp
+++ Linux_netdisk_client/Linux_netdisk_client/main.cpp
@@ -3,19 +3,57 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-int main()
-{
+#define MSG_TYPE_LOGIN 0
+#define MSG_TYPE_FILENAME 1
+//通讯协议的定制，两边要保持一致
+typedef struct msg{
+    int type; //协议类型 0登录协议包；1文件名传输包；
+    int flag; //标志位
+    char buffer[128]; //存放除文件名之外的内容
+    char fname[50]; //如果type是1，文件名传输包，那么fname里面就存放着文件名
+}MSG; //这个结构体会根据业务需求的不断变化，添加新的字段
+
+void net_disk_ui() {
+    //在服务器连接成功之后调用一下
+    system("clear");
+    printf("==========TCP网盘客户端================\n");
+    printf("============功能菜单===================\n");
+    printf("\t\t\t1.查询文件\n");
+    printf("\t\t\t2.下载文件\n");
+    printf("\t\t\t3.上传文件\n");
+    printf("\t\t\t0.退出系统\n");
+    printf("-----------------------------------------\n");
+    printf("请选择对应的操作");
+}
+
+//client也要创建线程。接收服务器发送过来的数据
+void* thread_func(void* arg){
+    int clinet_socket = *((int*)arg);
+    MSG recv_msg = { 0 };
+    int res;
+    while (1){
+        res = read(clinet_socket, &recv_msg, sizeof(MSG));        //用来接收服务器端发过来的数据
+        if (recv_msg.type == MSG_TYPE_FILENAME){
+            printf("filename:%s \n", recv_msg.fname);
+            memset(&recv_msg, 0, sizeof(MSG));
+        }
+    }
+}
+int main(){
     int client_socket;
     struct sockaddr_in server_addr; //用来填写要连接的服务器的ip地址和端口号
     char buffer[50] = { 0 };
     int res;
+    char c;     //c用来保存用户在键盘上输入的操作数字
+    pthread_t thread_id;
+    MSG send_msg = { 0 };
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket < 0){
         perror("client socket failed:");
         return 0;
     }
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("192.168.85.16"); //直接指定或者如果s和c在同一台电脑中可以用127.0.0.1
     server_addr.sin_port = htons(6666);
@@ -26,15 +64,28 @@ int main()
         return 0;
     }
     printf("client连接server成功\n");
+    net_disk_ui();
+    pthread_create(&thread_id, NULL, thread_func, &client_socket);
 
-    //客户端用write()往服务器发送数据，用户在客户端终端连续输入字符串，回车表示发送数据。
-    while (fgets(buffer, sizeof(buffer), stdin) != NULL){
-        res = write(client_socket, buffer, sizeof(buffer));//把buffer里面所有的数据发送给服务器,用write往服务器发送数据。 man write
-        printf("send bytes = %d\n", res);
-        memset(buffer, 0, sizeof(buffer));
-        res = read(client_socket, buffer, sizeof(buffer));
-        printf("receive from server info:%s\n", buffer);
-        memset(buffer, 0, sizeof(buffer));
+    /*原来的发送数据进行改造*/
+    while (1){
+        c = getchar(); //获取键盘输入
+        switch (c){
+        case '1':
+            send_msg.type = MSG_TYPE_FILENAME;
+            res = write(client_socket, &send_msg, sizeof(MSG));
+            if (res < 0){
+                perror("send msg error:");
+            }
+            memset(&send_msg, 0, sizeof(MSG));
+            break;
+        case '2':
+            break;
+        case '3':
+            break;
+        case '0':
+            return 0;
+        }
     }
 
     printf("%s 向你问好!\n", "Linux_netdisk_client");
--- Linux_netdisk_server/Linux_netdisk_server/main.cpp
+++ Linux_netdisk_server/Linux_netdisk_server/main.cpp
@@ -4,35 +4,77 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <pthread.h>
+#include <pthread.h>            /* thread */
+#include <dirent.h>             /* opendir */
 
+#define MSG_TYPE_LOGIN 0
+#define MSG_TYPE_FILENAME 1
+//通讯协议的定制，两边要保持一致
+typedef struct msg {
+    int type; //协议类型 0登录协议包；1文件名传输包；
+    int flag; //标志位
+    char buffer[128]; //存放除文件名之外的内容
+    char fname[50]; //如果type是1，文件名传输包，那么fname里面就存放着文件名
+}MSG; //这个结构体会根据业务需求的不断变化，添加新的字段
+
+void search_server_dir(int accept_socket){//把套接字作为参数传进来
+    struct dirent* dir = NULL; //定义一个存放目录信息的结构体
+    MSG info_msg = { 0 };
+    int res = 0;
+    DIR* dp = opendir("/home/linux");    //opendir是打开linux目录的api函数
+    info_msg.type = MSG_TYPE_FILENAME;
+    if (NULL == dp){
+        perror("open dir error:");
+        return;
+    }
+
+    while (1){
+        dir = readdir(dp);
+        if (NULL == dir){ //如果返回空值，表示目录全部读取完成。
+            break;
+        }
+
+        if (dir->d_name[0] != '.'){ //把.隐藏文件屏蔽
+            memset(info_msg.fname, 0, sizeof(info_msg.fname));  //处理前先清空一下
+            strcpy(info_msg.fname, dir->d_name); //d_name拷贝到fname 
+            res = write(accept_socket, &info_msg, sizeof(MSG));   //把每个文件名拷贝到info_msg结构体中，通过套接字发送出去
+            if (res < 0){
+                perror("send client error:");
+                return;
+            }
+        }
+    }
+}
+
+/*原来用buffer换成用结构体*/
 void* thread_fun(void* arg){
     int acpt_socket = *((int*)arg);     //accpet传递过来后，需要把它取出来,强制转换成指针
     int res;
     char buffer[50] = { 0 };
+    MSG recv_msg = { 0 };   //从buffer缓冲区转换成结构体大小
     while (1){
-        /*read函数就是接收客户端发来的数据，返回值表示实际从客户端那边收到的字节数。
-        buffer就是收到客户端数据后把数据存放的地址，sizeof(buffer)就是希望读取的字节数。*/
-        res = read(acpt_socket, buffer, sizeof(buffer));
-        printf("client read %s\n", buffer);
-        write(acpt_socket, buffer, res);
-        memset(buffer, 0, sizeof(buffer));
-        //服务器收到客户端数据后，原封不动把数据再回发给客户端。
+        res = read(acpt_socket, &recv_msg, sizeof(MSG));
+        if (res == 0) {
+            printf("客户端已经断开\n");
+            break;
+        }
+        if (recv_msg.type == MSG_TYPE_FILENAME){//说明客户端发过来的信息要的是目录
+            search_server_dir(acpt_socket);
+            memset(&recv_msg, 0, sizeof(MSG));
+        }
+        memset(&recv_msg, 0, sizeof(MSG));
     }
 }
-int main()
-{
+int main(){
     char buffer[50] = { 0 };
     int res = 0;
-
     printf("开始创建tcp服务器\n");
     int server_socket; 
     int accept_socket;
     pthread_t thread_id;    //线程编号
     //1.使用socket创建一个TCP流式套接字
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0)
-    {
+    if (server_socket < 0){
         perror("socket create error");
         return 0;
     }
@@ -70,6 +112,6 @@ int main()
         pthread_create(&thread_id, NULL, thread_fun, &accept_socket);
     }
 
-		printf("%s 向你问好!\n", "Linux_netdisk_server");
+	printf("%s 向你问好!\n", "Linux_netdisk_server");
 	return 0;
 }
